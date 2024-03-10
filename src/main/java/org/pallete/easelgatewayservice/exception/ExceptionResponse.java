e4166ba54f5813912ad1bff6b9350a223968cac5
Diger <kitten.diger@gmail.com>
2024-03-10 18:52:24 +0900

    chore: move to new repository

---
+++ src/main/java/org/pallete/easelgatewayservice/exception/ExceptionResponse.java
@@ -0,0 +1,12 @@
+package org.pallete.easelgatewayservice.exception;
+
+import lombok.Builder;
+
+@Builder
+public record ExceptionResponse(
+    String code,
+    String message,
+    String description
+) {
+
+}
---
+++ src/main/java/org/pallete/easelgatewayservice/util/RoundRobinList.java
@@ -0,0 +1,25 @@
+package org.pallete.easelgatewayservice.util;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import lombok.Getter;
+
+public class RoundRobinList<E> {
+
+    @Getter
+    private final List<E> data;
+    private final AtomicInteger currentIndex = new AtomicInteger(0);
+
+    public RoundRobinList(List<E> instanceInfos) {
+        this.data = instanceInfos;
+    }
+
+    public E next() {
+        if (data.isEmpty()) {
+            return null;
+        }
+
+        int index = currentIndex.getAndIncrement() % data.size();
+        return data.get(index);
+    }
+}
